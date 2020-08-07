f2fe3f2df64c5cc2cc8c50a2d077e886f5468435
AnesuChiodze <anesuemail@gmail.com>
2020-08-08 05:12:31 +1000

    Implimented advanced calcs section
    - Can handle advanced calculations requests
    - Currently can only do functions (f(x) etc)
    - Returns zeros, and derivatives for functions
    - Formatted output to be in MathML notation

--- extern.calculator.app/js/main.js
+++ extern.calculator.app/js/main.js
@@ -1,11 +1,30 @@
 var win = nw.Window.get();
 win.width = 670;
 
-//win.showDevTools();
+win.showDevTools();
 
-var e = nerdamer('x^2+2*(cos(x)+x*x)');
+		/*window.MathJax = {
+				"fast-preview": {
+       					disabled: true
+				},
+				AuthorInit: function() {
+					MathJax.Hub.Register.StartupHook('End', function() {
+						MathJax.Hub.processSectionDelay = 0
+						var demoSource = document.getElementById('advancedCalcsInput')
+						var demoRendering = document.getElementById('lols')
+						var math = MathJax.Hub.getAllJax('demoRendering')[0]
+						console.log("math: ",math);
+						demoSource.addEventListener('input', function() {
+							console.log("ok ok: ",demoSource.value);
+							//MathJax.Hub.Queue(['Text', math, demoSource.value])
+						})
+					})
+				}
+		}*/
 
-    console.log(e.text());
+var x = nerdamer('diff(x^4-2x^3, x)');
+
+    console.log(x.toString());
 
 
 var index = 0;
@@ -96,3 +115,87 @@ function exp() {
 		return numbers[index] + '.e+0';
 	});
 }
+
+
+/*Advanced Maths calculations*/
+
+/* Formats maths string so that it looks clean. However, this implimentation sucks as I'm not good with regex*/
+
+
+function getInputResults(input) {
+	if (input.indexOf("f(") == 0) {
+		//var variableToMonitor = input[2];
+		//$("#demoRendering").empty();
+		//$("#demoRendering").append("`"+input+"`");
+		var matches = input.match(/\((.*?)\)/);
+		if (matches) {
+			var variableToMonitor = matches[1];
+			if (input.split("=").length == 2) {
+				var equation = input.split("=")[1];
+				console.log("f(x) = ",variableToMonitor);
+				console.log("equation: ",equation);
+				try {
+					var def1 = nerdamer('diff('+equation+', '+variableToMonitor+')');
+					var def2 = nerdamer('diff('+equation+', '+variableToMonitor+', 2)');
+					var def3 = nerdamer('diff('+equation+', '+variableToMonitor+', 3)');
+					var solutions = nerdamer(equation).solveFor(variableToMonitor).toString().split(",");
+					console.log("def1: ",def1);
+					//console.log("zeros: ",solutions);
+					var zeros = '';
+					if (solutions.length > 0) {
+						zeros +='<h3 class="block-title">Zeros</h3>';
+						for (var i = 0; i < solutions.length; i++) {
+							zeros += '<p> `x_'+(i+1)+' = '+solutions[i]+'` </p>';
+						}
+					}
+					
+				$("#dynamicOutput").empty();
+					$("#dynamicOutput").append('<h1> `'+input+'` </h1>'+
+					zeros+
+					'<h3 class="block-title">Derivatives</h3>'+
+					"<p> `f'("+variableToMonitor+") = "+def1+'` </p>'+
+					"<p> `f''("+variableToMonitor+") = "+def2+'` </p>'+
+					"<p> `f'''("+variableToMonitor+") = "+def3+'` </p>'
+					);
+					
+					if ($("#advancedCalcOut").hasClass("hidden")) {
+						$("#tips").addClass("hidden");
+						$("#advancedCalcOut").removeClass("hidden");
+					}
+				} catch (e) {
+					$("#advancedCalcOut").addClass("hidden");
+					$("#tips").removeClass("hidden");
+				}
+				window.MathJax.Hub.Typeset();
+				
+			} else {
+				console.log("not fx yet")
+			}
+		}
+		
+		
+		
+	}
+}
+
+
+var oldInput = "";
+
+$('#advancedCalcsInput').change(function(){
+		console.log("input changed");
+	});
+
+		  	$("#advancedCalcsInput").keyup(function (e) {
+				  //var x = nerdamer('diff(x^4-2x^3, x)');
+				  
+				  //console.log(x.toString());
+				  if (oldInput != $("#advancedCalcsInput").val())
+				  	getInputResults($("#advancedCalcsInput").val());
+				
+				oldInput = $("#advancedCalcsInput").val()
+
+			if (e.keyCode == 13) { //Press ENTER
+
+				//enter pressed
+			}
+		});
